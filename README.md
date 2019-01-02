b1bafa77ab5e5d3457b694c0c919be89797bd6ab
Abby Stylianou <abigailstylianou@Abigails-MacBook-Pro.local>
2019-01-02 15:27:16 -0600

    updated main file

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Visualizing Deep Similarity Networks
 
+<p align="center">
+  <img width=50% src="https://www2.seas.gwu.edu/~astylianou/images/similarity-visualization/scene_similarity.png">
+</p>
+
 Download the pre-trained models:
 * Hotels: https://www2.seas.gwu.edu/~astylianou/similarity-visualization/hotels.tar.gz
 * Faces: https://www2.seas.gwu.edu/~astylianou/similarity-visualization/faces.tar.gz
--- visualize_similarity.py
+++ visualize_similarity.py
@@ -1,6 +1,6 @@
-import numpy as np
+from image_ops import *
+from similarity_ops import *
 import os
-import cv2
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from tensorflow.contrib.slim.nets import resnet_v2
@@ -13,10 +13,13 @@ from tensorflow.contrib.slim.nets import resnet_v2
 # https://www2.seas.gwu.edu/~astylianou/similarity-visualization/faces.tar.gz
 # https://www2.seas.gwu.edu/~astylianou/similarity-visualization/hotels.tar.gz
 # If you didn't download these into the main directory, you'll need to change the "pretrained_net" variable
-which_dataset = 'hotels'
+which_dataset = 'faces'
 pretrained_net = os.path.join(which_dataset,which_dataset)
 
+# The mean image for each dataset is included in the repository.
 mean_im_path = which_dataset + '_meanIm.npy'
+
+# For this demo, there are two example images for each of the datasets.
 im1_path = which_dataset+'1.jpg'
 im2_path = which_dataset+'2.jpg'
 
@@ -27,15 +30,8 @@ image_batch = tf.placeholder(tf.float32, shape=[2, 224, 224, 3])
 with slim.arg_scope(resnet_v2.resnet_arg_scope()):
     _, layers = resnet_v2.resnet_v2_50(image_batch, num_classes=128, is_training=True)
 
-# Load the pre-trained network.
-# NOTE: Pre-trained networks were trained w/ L2 normalization on output features.
-variables_to_restore = []
-for var in slim.get_model_variables():
-    excluded = False
-    if 'momentum' in var.op.name.lower():
-        excluded = True
-    if not excluded:
-        variables_to_restore.append(var)
+# Specify which variables to restore when loading the pretrained network.
+variables_to_restore = [var for var in slim.get_model_variables()]
 
 # Start a session.
 # If you need to specify a GPU, you can pass in a tf.ConfigProto() to tf.Session()
@@ -43,32 +39,27 @@ init_op = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init_op)
 
+# Load the pre-trained network.
+# NOTE: Pre-trained networks were trained w/ L2 normalization on output features.
 restore_fn = slim.assign_from_checkpoint_fn(pretrained_net,variables_to_restore)
 restore_fn(sess)
 
-# Specify the features that we'll be grabbing for our visualization
-last_conv = tf.squeeze(tf.get_default_graph().get_tensor_by_name("resnet_v2_50/postnorm/Relu:0")) # last convolutional layer
-output_feature = tf.squeeze(tf.get_default_graph().get_tensor_by_name("resnet_v2_50/pool5:0")) # pooled output feature
+# Grab the output of the last convolutional layer
+last_conv = tf.squeeze(tf.get_default_graph().get_tensor_by_name("resnet_v2_50/postnorm/Relu:0"))
 
-def preprocess_im(im_path,mean_im_path):
-    bgr_img = cv2.imread(im_path)
-    bgr_img = cv2.resize(bgr_img, (224,224))
-    mean_img = np.load(mean_im_path)
-    img = bgr_img - mean_img
-    return img
-
+# For this demo, load two example images from the same class.
 imgs = [preprocess_im(im,mean_im_path) for im in [im1_path,im2_path]]
 
-def compute_similarity(conv1,out1,conv2,out2):
-    conv1_normed = conv1 / np.linalg.norm(out1) / conv1.shape[0]
-    conv2_normed = conv2 / np.linalg.norm(out2) / conv2.shape[0]
+# Run the images through the network, get last conv features
+cv = sess.run(last_conv, feed_dict={image_batch: imgs})
 
-    im_similarity = np.zeros((conv1_normed.shape[0],conv1_normed.shape[0]))
-    for zz in range(conv1_normed.shape[0]):
-        repPx = np.matlib.repmat(conv1_normed[zz,:],conv1_normed.shape[0],1)
-        im_similarity[zz,:] = np.multiply(repPx,conv2_normed).sum(axis=1)
+# Compute the spatial similarity maps (returns a heatmap that's the size of the last conv layer)
+heatmap1, heatmap2 = compute_spatial_similarity(cv[0].reshape(-1,cv[0].shape[-1]),cv[1].reshape(-1,cv[1].shape[-1]))
 
-    return np.sum(im_similarity,axis=1), np.sum(im_similarity,axis=0)
+# Combine the images with the (interpolated) similarity heatmaps.
+im1_with_similarity = combine_image_and_heatmap(load_and_resize(im1_path),heatmap1)
+im2_with_similarity = combine_image_and_heatmap(load_and_resize(im2_path),heatmap2)
 
-cv,ff = sess.run([last_conv, output_feature], feed_dict={image_batch: imgs})
-sim1, sim2 = compute_similarity(cv[0].reshape(-1,cv[0].shape[-1]),ff[0],cv[1].reshape(-1,cv[1].shape[-1]),ff[1])
+# Merge the two images into a single image and save it out
+combined_image = pil_bgr_to_rgb(combine_horz([im1_with_similarity,im2_with_similarity]))
+combined_image.save(os.path.join('.',which_dataset+'_similarity.jpg'))
